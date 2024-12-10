b3dd399edcfb07304c740fc06c15c1b39a592c92
Tobiloba Adedeji <96334363+tobySolutions@users.noreply.github.com>
2024-12-10 16:26:52 +0100

    Update [shortCode].js

--- src/pages/[shortCode].js
+++ src/pages/[shortCode].js
@@ -1,4 +1,4 @@
-// export const runtime = "experimental-edge";
+export const runtime = "experimental-edge";
 
 export async function getServerSideProps(context) {
   const { shortCode } = context.params;
